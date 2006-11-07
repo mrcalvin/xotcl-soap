a82c10790dcc6f40ea077efcdefea3e4ce1905f1
Stefan Sobernig <ss@thinkersfoot.net>
2006-11-07 09:06:58 +0000

    Tested for WSDL 1.1 schema compliance by means of mindreef wsdl validator; corrected minor issues

--- tcl/xosoap-wsdl-procs.tcl
+++ tcl/xosoap-wsdl-procs.tcl
@@ -1,7 +1,7 @@
 ad_library {
 	
-	WSDL 1.1 support for xorb's SOAP-plugin, xosoap
-	see http://www.w3.org/TR/wsdl
+    WSDL 1.1 support for xorb's SOAP-plugin, xosoap
+    see http://www.w3.org/TR/wsdl
 		
     @author stefan.sobernig@wu-wien.ac.at
     @creation-date August 18, 2005
@@ -9,193 +9,199 @@ ad_library {
 
  }
  
-	 ::xosoap::MessageHandler ad_instproc getWSDL -servicePointer:required {} {
+::xosoap::MessageHandler ad_instproc getWSDL -servicePointer:required {} {
 	
-	 	# lookup: contract composite
-	 	set bundle [XorbContainer do ::xorb::SCBroker getContract -serialized -name $servicePointer]
+    # lookup: contract composite
+    set bundle [XorbContainer do ::xorb::SCBroker getContract -serialized -name $servicePointer]
 	 	
+    #my log "bundle=$bundle"
 	 	
-	 	set contractPointer [lindex $bundle 0]
-	 	
-	 	#::xorb::MessageType addOperations {accept}
-	 	
-	 	eval [lindex $bundle 1]
+    set contractPointer [lindex $bundle 0]
 	 
-	 	# unveal / create WSDLVisitor
-	 	
-	 	set visitor [::xosoap::visitor::WsdlBuilderVisitor new -volatile]
-	 	$contractPointer accept $visitor	
-	 	
-	 	# return WSDL structure
-	 	return "ns_return 200 text/xml {[$visitor asXML]}"
-	 }
+    #::xorb::MessageType addOperations {accept}
+    ::xotcl::Class ::xorb::Retrievable 
+    eval [lindex $bundle 1]
 	 
-	 namespace eval xosoap::visitor {
-	 
-		 ::xotcl::Class WsdlBuilderVisitor
-		 
-		 WsdlBuilderVisitor ad_instproc init {} {} {
-		 		
-		 		my instvar ns doc url xsdTypes
-		 		
-		 		set url "[ns_conn location][ns_urldecode [ns_conn url]]"
-				set ns(tns) 	"$url?wsdl"
-		 		set ns(soap) "http://schemas.xmlsoap.org/wsdl/soap/"
-		 		set ns(xsd) "http://www.w3.org/2000/10/XMLSchema"
+ 	 	# unveal / create WSDLVisitor
+	 	
+    set visitor [::xosoap::visitor::WsdlBuilderVisitor new -volatile]
+    $contractPointer accept $visitor	
+	 	
+    # return WSDL structure
+    return "ns_return 200 text/xml {[$visitor asXML]}"
+}
+
+namespace eval xosoap::visitor {
+    
+    ::xotcl::Class WsdlBuilderVisitor
+    
+    WsdlBuilderVisitor ad_instproc init {} {} {
+	
+	my instvar ns doc url xsdTypes
+	
+	set url "[ns_conn location][ns_urldecode [ns_conn url]]"
+	set ns(tns) 	"$url?wsdl"
+	set ns(soap) "http://schemas.xmlsoap.org/wsdl/soap/"
+	set ns(soap-enc) "http://schemas.xmlsoap.org/soap/encoding/"
+	set ns(xsd) "http://www.w3.org/2001/XMLSchema"
+	set ns(wsdl) "http://schemas.xmlsoap.org/wsdl/"
+	
+	
+	set xsdTypes(string) 	"xsd:string"
+	set xsdTypes(integer)	"xsd:integer"
 		 		
 	
-		 		set xsdTypes(string) 	"xsd:string"
-		 		set xsdTypes(integer)	"xsd:integer"
-		 		
-		 		
-		 		
-		 		set doc [dom createDocument "definitions"]
-		 		
-		 		# add ns declarations
-		 		foreach k [array names ns] {
-		 		
-		 			[$doc documentElement] setAttribute "xmlns:$k" $ns($k)
-		 		}
-		 		
-		 }
-		 
-		 WsdlBuilderVisitor ad_instproc visit {obj} {} {
+	
+	set doc [dom createDocumentNS $ns(wsdl) "wsdl:definitions"]
+	
+	# add ns declarations
+	foreach k [array names ns] {
+	    
+	    [$doc documentElement] setAttribute "xmlns:$k" $ns($k)
+	}
+	
+    }
+    
+    WsdlBuilderVisitor ad_instproc visit {obj} {} {
+	if {[my isobject $obj]} {
+	    eval my [namespace tail [$obj info class]] $obj 
+	}
+	
+    }
+    
+    WsdlBuilderVisitor ad_instproc asXML {} {} {
+	
+	my instvar doc
+	return [$doc asXML -doctypeDeclaration true]
+				
+    }
+    
+    WsdlBuilderVisitor ad_instproc ServiceContract {serviceContract} {} {
+	
+	my instvar doc ns url elPortType
+	
+	set current [$doc getElementsByTagName "wsdl:definitions"]
+	
+	# add attrs to definitions el (doc, name attrs)
+	my log current=$current,sc=$serviceContract
+	$current setAttribute "name" [$serviceContract label]
+	$current setAttribute "targetNamespace" $ns(tns)
+	# append service element
+	set elService [$current appendChild [$doc createElementNS $ns(wsdl) "wsdl:service"]]
+	$elService setAttribute "name" [$serviceContract label]
+	set elDoc [$elService appendChild [$doc createElementNS $ns(wsdl) "wsdl:documentation"]]
+	$elDoc appendChild [$doc createTextNode [$serviceContract description]]
+	# append binding element
+	set elBinding [$current insertBefore [$doc createElementNS $ns(wsdl) "wsdl:binding"] $elService]
+	$elBinding setAttribute "name" "[$serviceContract label]SoapBinding"
+	$elBinding setAttribute "type" "tns:[$serviceContract label]PortType"
+	set el1SoapBind [$elBinding appendChild [$doc createElementNS $ns(soap) "soap:binding"]]
+	$el1SoapBind setAttribute "style" "rpc"
+	$el1SoapBind setAttribute "transport" "http://schemas.xmlsoap.org/soap/http"
+	# append port element and add port-binding info to service el
+	set elPortType [$current insertBefore [$doc createElementNS $ns(wsdl) "wsdl:portType"] $elBinding]
+	$elPortType setAttribute "name" "[$serviceContract label]PortType"
+	set el1Port [$elService appendChild [$doc createElementNS $ns(wsdl) "wsdl:port"]]
+	$el1Port setAttribute "name" "[$serviceContract label]Port"
+	$el1Port setAttribute "binding" "tns:[$serviceContract label]SoapBinding"; # tns:[$serviceContract label]SoapBinding?
+	set el2Address [$el1Port appendChild [$doc createElementNS $ns(soap) "soap:address"]]
+	$el2Address setAttribute "location" $url
+    }
+    
+    WsdlBuilderVisitor ad_instproc Operation {op} {} {
+	
+	my instvar doc operation ns url
+	set operation $op
+	
+	set current [$doc getElementsByTagName "wsdl:portType"]
+	set el1Operation [$current appendChild [$doc createElementNS $ns(wsdl) "wsdl:operation"]]
+	$el1Operation setAttribute "name" [$operation label]
+	
+	set current [$doc getElementsByTagName "wsdl:binding"]
+	set el1Operation [$current appendChild [$doc createElementNS $ns(wsdl) "wsdl:operation"]]
+	$el1Operation setAttribute "name" [$operation label]
+	set el2SoapOp [$el1Operation appendChild [$doc createElementNS $ns(soap) "soap:operation"]]
+	$el2SoapOp setAttribute "soapAction" ${url}/[$operation label]
 
-				if {[my isobject $obj]} {
-					eval my [namespace tail [$obj info class]] $obj 
-				}
+	::xorb::aux::SortableTypedComposite instfilter add compositeFilter
+	#my log "objfilter: [$operation info filter], instfilters: [[$operation info class] info instfilter]"
+    }
+    
+    WsdlBuilderVisitor ad_instproc Input {input} {} {
+	
+	my instvar doc url operation ns elPortType
+	set operationName [$operation label]
+	#message element
+	set current [$doc documentElement]
+	set elMessage [$current insertBefore [$doc createElementNS $ns(wsdl) "wsdl:message"] $elPortType]
+	$elMessage setAttribute "name" "${operationName}Input"
+	# portType/input
+	set xpath [subst {/wsdl:definitions/wsdl:portType/wsdl:operation\[@name='$operationName'\]}]
+	my log "+++xml=[$current asXML]"
 
-		}
+	set current [$current selectNodes $xpath]
+	set el2Input [$current appendChild [$doc createElementNS $ns(wsdl) "wsdl:input"]]
+	$el2Input setAttribute "message" "tns:${operationName}Input"
+	
+	# binding/input
+	
+	set current [$doc documentElement]
+	set current [$current selectNodes "/wsdl:definitions/wsdl:binding/wsdl:operation\[@name='$operationName'\]"]
+	my log +2current=$current,doc=$doc
+	set el2Input [$current appendChild [$doc createElementNS $ns(wsdl) "wsdl:input"]]
+	set el3SoapBody [$el2Input appendChild [$doc createElementNS $ns(soap) "soap:body"]]
+	$el3SoapBody setAttribute "use" "literal"
+	$el3SoapBody setAttribute "namespace" $url
+	$el3SoapBody setAttribute "encodingStyle" $ns(soap-enc)
+	
+	
+    }
+    
+    WsdlBuilderVisitor ad_instproc Output {output} {} {
+	
+	
+	my instvar doc url operation ns elPortType
+	set operationName [$operation label]
+	
+	#message element
+	set current [$doc getElementsByTagName "wsdl:definitions"]
+	set elMessage [$current insertBefore [$doc createElementNS $ns(wsdl) "wsdl:message"] $elPortType]
+	$elMessage setAttribute "name" "${operationName}Output"
+	
+	# portType/input
+	
+	set current [[$doc documentElement] selectNodes "/wsdl:definitions/wsdl:portType/wsdl:operation\[@name='[$operation label]'\]"]
+	set el2Input [$current appendChild [$doc createElementNS $ns(wsdl) "wsdl:output"]]
+	$el2Input setAttribute "message" "tns:${operationName}Output"
+	
+	# binding/input
+	
+	set current [[$doc documentElement] selectNodes "/wsdl:definitions/wsdl:binding/wsdl:operation\[@name='[$operation label]'\]"]
+	set el2Input [$current appendChild [$doc createElementNS $ns(wsdl) "wsdl:output"]]
+	set el3SoapBody [$el2Input appendChild [$doc createElementNS $ns(soap) "soap:body"]]
+	$el3SoapBody setAttribute "use" "literal"
+	$el3SoapBody setAttribute "namespace" $url
+	$el3SoapBody setAttribute "encodingStyle" $ns(soap-enc)
 		
-		WsdlBuilderVisitor ad_instproc asXML {} {} {
-					
-				my instvar doc
-				return [$doc asXML -doctypeDeclaration true]
-				
-		}
-		
-		WsdlBuilderVisitor ad_instproc ServiceContract {serviceContract} {} {
-				
-				my instvar doc ns url
-				
-				set current [$doc getElementsByTagName "definitions"]
-				
-				# add attrs to definitions el (doc, name attrs)
-				$current setAttribute "name" [$serviceContract label]
-				$current setAttribute "targetNamespace" $ns(tns)
-				# append service element
-				set elService [$current appendChild [$doc createElement "service"]]
-				$elService appendChild [$doc createTextNode [$serviceContract description]]
-				# append binding element
-				set elBinding [$current appendChild [$doc createElement "binding"]]
-				$elBinding setAttribute "name" "[$serviceContract label]SoapBinding"
-				$elBinding setAttribute "type" "tns:[$serviceContract label]PortType"
-				set el1SoapBind [$elBinding appendChild [$doc createElement "soap:binding"]]
-				$el1SoapBind setAttribute "stlye" "rpc"
-				$el1SoapBind setAttribute "transport" "http://schemas.xmlsoap.org/soap/http"
-				# append port element and add port-binding info to service el
-				set elPortType [$current appendChild [$doc createElement "portType"]]
-				$elPortType setAttribute "name" "[$serviceContract label]PortType"
-				set el1Port [$elService appendChild [$doc createElement "port"]]
-				$el1Port setAttribute "name" "[$serviceContract label]Port"
-				$el1Port setAttribute "binding" "tns:[$serviceContract label]Binding"; # tns:[$serviceContract label]SoapBinding?
-				set el2Address [$el1Port appendChild [$doc createElement "soap:address"]]
-				$el2Address setAttribute "location" $url
-			
-		
-		}
-	 
-	 	WsdlBuilderVisitor ad_instproc Operation {op} {} {
-		
-				my instvar doc operation
-				set operation $op
-			
-				set current [$doc getElementsByTagName "portType"]
-				set el1Operation [$current appendChild [$doc createElement "operation"]]
-				$el1Operation setAttribute "name" [$operation label]
-				
-				set current [$doc getElementsByTagName "binding"]
-				set el1Operation [$current appendChild [$doc createElement "operation"]]
-				$el1Operation setAttribute "name" [$operation label]
-				
-				::xorb::aux::SortableTypedComposite instfilter add compositeFilter
-				my log "objfilter: [$operation info filter], instfilters: [[$operation info class] info instfilter]"
-		}
- 		
- 		WsdlBuilderVisitor ad_instproc Input {input} {} {
-		
-				my instvar doc url operation
-				set operationName [$operation label]
-				my log "+++ opName: $operationName"
-				#message element
-				set current [$doc getElementsByTagName "definitions"]
-				my log "+++ current:$current"
-				set elMessage [$current appendChild [$doc createElement "message"]]
-				$elMessage setAttribute "name" "${operationName}Input"
-				
-				# portType/input
-				
-				set current [[$doc documentElement] selectNodes "/definitions/portType/operation\[@name='[$operation label]'\]"]
-				set el2Input [$current appendChild [$doc createElement "input"]]
-				$el2Input setAttribute "message" "tns:${operationName}Input"
-				
-				# binding/input
-				
-				set current [[$doc documentElement] selectNodes "/definitions/binding/operation\[@name='[$operation label]'\]"]
-				set el2Input [$current appendChild [$doc createElement "input"]]
-				set el3SoapBody [$el2Input appendChild [$doc createElement "soap:body"]]
-				$el3SoapBody setAttribute "use" "encoded"
-				$el3SoapBody setAttribute "namespace" $url
-				$el3SoapBody setAttribute "encodingStyle" "http://schemas.xmlsoap.org/soap/encoding/"
-				
-				
-		}
-		
-		WsdlBuilderVisitor ad_instproc Output {output} {} {
-		
-		
-				my instvar doc url operation
-				set operationName [$operation label]
-				
-				#message element
-				set current [$doc getElementsByTagName "definitions"]
-				set elMessage [$current appendChild [$doc createElement "message"]]
-				$elMessage setAttribute "name" "${operationName}Output"
-				
-				# portType/input
-				
-				set current [[$doc documentElement] selectNodes "/definitions/portType/operation\[@name='[$operation label]'\]"]
-				set el2Input [$current appendChild [$doc createElement "output"]]
-				$el2Input setAttribute "message" "tns:${operationName}Output"
-				
-				# binding/input
-				
-				set current [[$doc documentElement] selectNodes "/definitions/binding/operation\[@name='[$operation label]'\]"]
-				set el2Input [$current appendChild [$doc createElement "output"]]
-				set el3SoapBody [$el2Input appendChild [$doc createElement "soap:body"]]
-				$el3SoapBody setAttribute "use" "encoded"
-				$el3SoapBody setAttribute "namespace" $url
-				$el3SoapBody setAttribute "encodingStyle" "http://schemas.xmlsoap.org/soap/encoding/"
-		
-		}
-		
-		WsdlBuilderVisitor ad_instproc Argument {argument} {} {
-				
-				my instvar doc operation xsdTypes
-				set current [[$doc documentElement] selectNodes "/definitions/message\[@name='[$operation label]Input'\]"]
-				set el2Argument [$current appendChild [$doc createElement "part"]]
-				$el2Argument setAttribute "name" [$argument label]
-				$el2Argument setAttribute "type" $xsdTypes([$argument datatype])
-				
-		}
-		
-		WsdlBuilderVisitor ad_instproc ReturnValue {returnValue} {} {
-		
-				my instvar doc operation xsdTypes
-				set current [[$doc documentElement] selectNodes "/definitions/message\[@name='[$operation label]Output'\]"]
-				set el2Argument [$current appendChild [$doc createElement "part"]]
-				$el2Argument setAttribute "name" [$returnValue label]
-				$el2Argument setAttribute "type" $xsdTypes([$returnValue datatype])
-		}
- 	}
+    }
+    
+    WsdlBuilderVisitor ad_instproc Argument {argument} {} {
+	
+	my instvar doc operation xsdTypes ns
+	set current [[$doc documentElement] selectNodes "/wsdl:definitions/wsdl:message\[@name='[$operation label]Input'\]"]
+	set el2Argument [$current appendChild [$doc createElementNS $ns(wsdl) "wsdl:part"]]
+	$el2Argument setAttribute "name" [$argument label]
+	$el2Argument setAttribute "type" $xsdTypes([$argument datatype])
+	
+    }
+    
+    WsdlBuilderVisitor ad_instproc ReturnValue {returnValue} {} {
+	
+	my instvar doc operation xsdTypes ns
+	set current [[$doc documentElement] selectNodes "/wsdl:definitions/wsdl:message\[@name='[$operation label]Output'\]"]
+	set el2Argument [$current appendChild [$doc createElementNS $ns(wsdl) "wsdl:part"]]
+	$el2Argument setAttribute "name" [$returnValue label]
+	$el2Argument setAttribute "type" $xsdTypes([$returnValue datatype])
+    }
+}
  	
