c4039a63eac94fe975bab69e56b2de5dac1df1cb
Stefan Sobernig <ss@thinkersfoot.net>
2007-03-19 08:59:38 +0000

    cleanup from 0.2 to 0.3

--- tcl/examples-client-init.tcl
+++
@@ -1,5 +0,0 @@
-
-SOAP::create getPoem \
-	-uri "urn:xmethods-getPoem" \
-	-proxy "http://localhost:8070/xosoap/services/Poem" \
-	-params { "statement" "string" }	
--- tcl/tsoap/babelfish-init.tcl
+++
@@ -1,9 +0,0 @@
-SOAP::configure -transport http -proxy {}
-
-SOAP::create translate \
-    -proxy {http://services.xmethods.net:80/perl/soaplite.cgi} \
-    -uri "urn:xmethodsBabelFish\#BabelFish" \
-    -params { translationmode string sourcedata string }
-
-# example:
-# set english [translate de_en "Hallo Welt, Guten Tag"]
